ae9d1835cde315efcc61315731917aa88f496023
LAPTOP-HLTN7FRC\Alexander <alexander.glandberger@outlook.com>
2019-05-15 12:19:09 +0200

    startet with addTodo-list method
    startet with addTodo-list method

---
+++ TodoMVC.js
@@ -0,0 +1,24 @@
+// function addTodo(){
+    //      var txtVal = document.getElementById('Insertinfo').nodeValue,
+    //      listNode = document.getElementById('list'),
+    //      liNode = document.createElement("Li"),
+    //      txtNode = document.createTextNode(txtVal);
+    //      liNode.appendChild(txtNode);
+    //      listNode.appendChild(liNode);
+    
+    // }
+    addTodo();
+
+    function addTodo(){
+        const form = document.querySelector("form");
+        form.onsubmit = event => {
+            event.preventDefault();
+        
+            const toDoInput = document.getElementById("Insertinfo");
+            const newToDo = document.createElement("Li");
+            newToDo.textContent = toDoInput.value;
+            document.body.appendChild(newToDo);
+            document.getElementById('Insertinfo').value = '';
+        }
+
+}
