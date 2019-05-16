c8c403807314aef5c376f20b0e9f9cea4edc29a2
LAPTOP-HLTN7FRC\Alexander <alexander.glandberger@outlook.com>
2019-05-16 13:14:36 +0200

    added more function to addTodo-function

--- TodoMVC.js
+++ TodoMVC.js
@@ -15,9 +15,19 @@
             event.preventDefault();
         
             const toDoInput = document.getElementById("Insertinfo");
-            const newToDo = document.createElement("ul");
-            newToDo.textContent = toDoInput.value;
-            document.body.appendChild(newToDo);
+            const getUl = document.querySelector("#ullist");
+            const newLi = document.createElement("li");
+            const newInput = document.createElement("input");
+            const newLabel = document.createElement("label");
+            const newButton = document.createElement("button");
+
+            console.log("#ulList");
+            console.log(getUl);
+            getUl.appendChild(newLi);
+            newLi.appendChild(newInput, newLabel, newButton);
+
+            getUl.textContent = toDoInput.value;
+            //document.body.appendChild(newToDo);
             document.getElementById('Insertinfo').value = '';
         }
 
