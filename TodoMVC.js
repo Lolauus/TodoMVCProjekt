d5cfb034a7beb65e908677a2b3ee17eac8f69d65
LAPTOP-HLTN7FRC\Alexander <alexander.glandberger@outlook.com>
2019-05-16 14:07:14 +0200

    Revert "Update TodoMVC.js"
    This reverts commit c82a5bab41c9fde0b981a4263dcec18d692ced1f, reversing
    changes made to 7f581c77742c3712abe70daf4fe6f2cec6cdd364.

--- TodoMVC.js
+++ TodoMVC.js
@@ -15,7 +15,6 @@
         form.onsubmit = event => {
             event.preventDefault();
         
-<<<<<<< HEAD
             let toDoInput = document.getElementById("Insertinfo");
             let getUl = document.querySelector("#ullist");
             let newLi = document.createElement("li");
@@ -27,21 +26,6 @@
             newLi.appendChild(newInput);
             newLi.appendChild(newLabel);
             newLi.appendChild(newButton);
-=======
-            const toDoInput = document.getElementById("Insertinfo");
-<<<<<<< HEAD
-            const newToDo = document.createElement("ul");
-            const newList = document.createElement("li");
-            newToDo.appendChild(newList);
-            newToDo.textContent = toDoInput.value;
-            document.body.appendChild(newToDo);
-=======
-            const getUl = document.querySelector("#ullist");
-            const newLi = document.createElement("li");
-            const newInput = document.createElement("input");
-            const newLabel = document.createElement("label");
-            const newButton = document.createElement("button");
->>>>>>> 0a56d2de83d16c4c20553d8b16e418ee9e17fec0
 
             getUl.appendChild(newLi);
 
@@ -49,13 +33,7 @@
 
             getUl.textContent = toDoInput.value;
             //document.body.appendChild(newToDo);
->>>>>>> c8c403807314aef5c376f20b0e9f9cea4edc29a2
             document.getElementById('Insertinfo').value = '';
-        }
-        if(document.getElementById("Insertinfo" === "")){
-
-            alert("You must write something, bitch ")
-
         }
 
 }
