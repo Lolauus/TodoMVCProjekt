0083ad42c6998d7927b6e5907968f5c0013484ac
Viktor Persson <Viktor.persson91@gmail.com>
2019-05-16 14:06:37 +0200

    Revert "gea"
    This reverts commit 0a56d2de83d16c4c20553d8b16e418ee9e17fec0, reversing
    changes made to 8484f579a65ca07995ff8c103c1a98199ed30c35.

--- TodoMVC.js
+++ TodoMVC.js
@@ -15,27 +15,11 @@
             event.preventDefault();
         
             const toDoInput = document.getElementById("Insertinfo");
-<<<<<<< HEAD
             const newToDo = document.createElement("ul");
             const newList = document.createElement("li");
             newToDo.appendChild(newList);
             newToDo.textContent = toDoInput.value;
             document.body.appendChild(newToDo);
-=======
-            const getUl = document.querySelector("#ullist");
-            const newLi = document.createElement("li");
-            const newInput = document.createElement("input");
-            const newLabel = document.createElement("label");
-            const newButton = document.createElement("button");
-
-            console.log("#ulList");
-            console.log(getUl);
-            getUl.appendChild(newLi);
-            newLi.appendChild(newInput, newLabel, newButton);
-
-            getUl.textContent = toDoInput.value;
-            //document.body.appendChild(newToDo);
->>>>>>> c8c403807314aef5c376f20b0e9f9cea4edc29a2
             document.getElementById('Insertinfo').value = '';
         }
         if(document.getElementById("Insertinfo" === "")){
